ce5d004e8874496ce7dacbd853bb1a7204c6eb99
Matthew Chuong <mstchuong@yahoo.com>
2023-10-04 22:17:28 -0400

    Create AddTodo with Realm

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {config, GluestackUIProvider, Text, Center} from '@gluestack-ui/themed';
+import React, {useState} from 'react';
+import {
+  config,
+  GluestackUIProvider,
+  Text,
+  Center,
+  Button,
+  ButtonText,
+} from '@gluestack-ui/themed';
 // import {RealmProvider} from './realm/realmProvider';
 import {createRealmContext} from '@realm/react';
 import Realm, {ObjectSchema} from 'realm';
@@ -11,20 +18,21 @@ class Todo extends Realm.Object<Todo> {
 
   static schema: ObjectSchema = {
     name: 'Todo',
+    primaryKey: '_id',
     properties: {
-      _id: 'int',
+      _id: 'objectId',
       description: {type: 'string'},
       done: 'bool',
     },
-    primaryKey: '_id',
   };
 }
 
 const realmConfig: Realm.Configuration = {
   schema: [Todo],
+  schemaVersion: 2,
 };
 
-export const {RealmProvider, useRealm, useObject, useQuery} =
+const {RealmProvider, useRealm, useObject, useQuery} =
   createRealmContext(realmConfig);
 
 export default function App() {
@@ -33,8 +41,41 @@ export default function App() {
       <GluestackUIProvider config={config.theme}>
         <Center h={'100%'}>
           <Text>Hello World!</Text>
+          <CreateTodo />
+          <TodoList />
         </Center>
       </GluestackUIProvider>
     </RealmProvider>
   );
 }
+
+const TodoList = () => {
+  // const [allTodos, setAllTodos] = useState<Realm.Results<Todo>>();
+  const todos = useQuery(Todo);
+
+  return (
+    <Center>
+      {todos.map(t => (
+        <Text>{t.description}</Text>
+      ))}
+    </Center>
+  );
+};
+
+const CreateTodo = () => {
+  const realm = useRealm();
+  const addTodo = (description: string) => {
+    realm.write(() => {
+      realm.create('Todo', {
+        _id: new Realm.BSON.ObjectId(),
+        description: description,
+        done: false,
+      });
+    });
+  };
+  return (
+    <Button onPress={() => addTodo('test')}>
+      <ButtonText>Create Todo</ButtonText>
+    </Button>
+  );
+};
