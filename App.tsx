17b7789569f26ccfdccc14b45d8b52944ba6d395
Matthew Chuong <mstchuong@yahoo.com>
2023-10-17 22:08:31 -0400

    Add PushNotification test

--- App.tsx
+++ App.tsx
@@ -8,12 +8,35 @@ import {
   Heading,
 } from '@gluestack-ui/themed';
 import {RealmProvider} from './realm/realmProvider';
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 const TodoList = lazy(() => import('./components/TodoList'));
 const CreateTodoForm = lazy(() => import('./pages/CreateTodoForm'));
 const TodoProgress = lazy(() => import('./components/TodoProgress'));
 
 export default function App() {
+  const getCorrectDate = () => {
+    const date = new Date();
+    date.setDate(date.getDate());
+    date.setHours(22);
+    date.setMinutes(5);
+    return date;
+  };
+
+  PushNotificationIOS.addNotificationRequest({
+    body: 'test',
+    id: 'test',
+    title: 'title',
+    subtitle: 'subtitile',
+    badge: 1,
+    isCritical: true,
+    fireDate: getCorrectDate(),
+    repeats: true,
+    repeatsComponent: {
+      minute: true,
+    },
+  });
+
   return (
     <RealmProvider>
       <GluestackUIProvider config={config.theme}>
